00d5a63b118859f83391107701f02bf3063bc34b
Janne Tolppanen <janne.ks@gmail.com>
2023-04-10 21:48:44 +0300

    Midway of adding noteservice and not sure what is gonna break

--- osa2/taulukot_esimerkki/src/App.js
+++ osa2/taulukot_esimerkki/src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
+import Noteservice from './services/notes'
 
 const App = (props) => {
   const [notes, setNotes] = useState([])
@@ -17,7 +18,7 @@ const App = (props) => {
         setNotes(response.data)
       })
   }
-  
+
 
   useEffect(hook, [])
 
@@ -28,8 +29,23 @@ const App = (props) => {
       important: Math.random() > 0.5,
       id: notes.length + 1,
     }
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    axios.post('http://localhost:3001/notes', noteObject)
+    .then(response => {
+      setNotes(notes.concat(response.data))
+      setNewNote('')
+    })
+  }
+
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important}
+
+    axios
+      .put(url, changedNote)
+      .then(response => {
+        setNotes(notes.map(note => note.id !== id ? note : response.data))
+      })
   }
 
   const handleNoteChange = (event) => {
@@ -51,7 +67,10 @@ const App = (props) => {
       </div>
       <ul>
         {notesToShow.map(note =>
-          <Note key={note.id} note={note} />
+          <Note 
+          key={note.id} 
+          note={note} 
+          toggleImportance={() => toggleImportanceOf(note.id)}/>
         )}
       </ul>
       <form onSubmit={addNote}>
