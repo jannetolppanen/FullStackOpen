cf2586e93022f308df71024486aeaa51301e791d
Janne Tolppanen <janne.ks@gmail.com>
2023-04-18 14:37:18 +0300

    footer ja virheviesti lisätty puhelinluetteloon

--- osa2/taulukot_esimerkki/src/App.js
+++ osa2/taulukot_esimerkki/src/App.js
@@ -38,13 +38,12 @@ const App = () => {
     setNewNote(event.target.value)
   }
 
-  const toggleImportanceOf = (id) => {
+  const toggleImportanceOf = id => {
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
-  
+
     noteService
-      .update(id, changedNote)
-      .then(returnedNote => {
+      .update(id, changedNote).then(returnedNote => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch(error => {
