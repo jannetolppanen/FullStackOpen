c07a264970c13304f52fd4c62c32f9f04292fa64
Janne Tolppanen <janne.ks@gmail.com>
2023-05-13 15:16:02 +0300

    Remove and update added. This is just before starting 3.15

--- esimerkkiharjoitukset/notes-backend/index.js
+++ esimerkkiharjoitukset/notes-backend/index.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const Note = require('./models/note')
+require('dotenv').config()
 
+const Note = require('./models/note')
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -13,18 +13,26 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
+
+
 
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
 app.use(express.static('build'))
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
 
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
@@ -32,20 +40,13 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (body.content === undefined) {
+    console.log('body:' +body.content)
     return response.status(400).json({ error: 'content missing' })
   }
-
   const note = new Note({
     content: body.content,
     important: body.important || false,
@@ -56,20 +57,44 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+  Note.findByIdAndRemove(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
+})
 
-  response.status(204).end()
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 app.use(unknownEndpoint)
+// tämä tulee kaikkien muiden middlewarejen rekisteröinnin jälkeen!
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
