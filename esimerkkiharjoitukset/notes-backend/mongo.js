a73532644f150f0e1189f5ef661abe8b581140b2
Janne Tolppanen <janne.ks@gmail.com>
2023-05-17 20:58:44 +0300

    example app notes updated part 4

--- esimerkkiharjoitukset/notes-backend/mongo.js
+++ esimerkkiharjoitukset/notes-backend/mongo.js
@@ -8,31 +8,35 @@ if (process.argv.length<3) {
 const password = process.argv[2]
 
 const url =
-  `mongodb+srv://janne:${password}@cluster0.qrchvix.mongodb.net/noteApp?retryWrites=true&w=majority`
+  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/?retryWrites=true&w=majority`
 
-mongoose.set('strictQuery', false)
+mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
   content: String,
+  date: Date,
   important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
-  content: 'If you see this its from the Atlas DB',
+  content: 'Mongoose makes things easy',
+  date: new Date(),
   important: true,
 })
 
-// note.save().then(result => {
-//   console.log(result)
-//   mongoose.connection.close()
-// })
+/*
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
+*/
 
-Note.find({ important: true }).then(result => {
-    result.forEach(note => {
-      console.log(note)
-    })
-    mongoose.connection.close()
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
   })
+  mongoose.connection.close()
+})
