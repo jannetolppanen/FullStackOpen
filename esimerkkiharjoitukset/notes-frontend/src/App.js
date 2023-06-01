1a2642645471eb810e585b63cc5d15818b0b8c04
Janne Tolppanen <janne.ks@gmail.com>
2023-06-01 20:37:18 +0300

    started part 5 example notes app. added nes css just for lols

--- esimerkkiharjoitukset/notes-frontend/src/App.js
+++ esimerkkiharjoitukset/notes-frontend/src/App.js
@@ -9,6 +9,8 @@ const App = () => {
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
 
   useEffect(() => {
     noteService
@@ -60,12 +62,40 @@ const App = () => {
         })
     }
 
+    const handleLogin = (event) => {
+      event.preventDefault()
+      console.log('logging in with', username, password)
+    }
+
   return (
     <div>
       <h1>Notes app</h1>
       <Notification message={errorMessage} />
+
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
       <div>
-        <button onClick={() => setShowAll(!showAll)}>
+        <button class="nes-btn is-success" onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
         </button>
       </div> 
--- esimerkkiharjoitukset/notes-frontend/src/components/Note.js
+++ esimerkkiharjoitukset/notes-frontend/src/components/Note.js
@@ -3,10 +3,14 @@ const Note = ({ note, toggleImportance }) => {
     ? 'make not important' 
     : 'make important';
 
+  const buttonStyle = note.important
+  ? 'nes-btn is-primary'
+  : 'nes-btn is-warning'
+
   return (
     <li className='note'>
       {note.content} 
-      <button onClick={toggleImportance}>{label}</button>
+      <button class={buttonStyle} onClick={toggleImportance}>{label}</button>
     </li>
   )
 }
